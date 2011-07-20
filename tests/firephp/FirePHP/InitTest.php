bae54de6f42bfd612d09291b5927327966d00481
cadorn <christoph@christophdorn.com>
2011-07-20 13:45:00 -0700

    FirePHP tests

---
+++ tests/firephp/FirePHP/InitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class FirePHP_InitTest extends PHPUnit_Framework_TestCase
+{
+    public function testConnect()
+    {
+        $console = FirePHP::to('process')->console();
+
+        $console->label('Label 1')->log('Hello World 1');
+        $console->expand()->label('Label 2')->log('Hello World 2');
+
+        $console->show();
+    }
+}
