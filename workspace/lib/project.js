591b0f523d39b607f9ed788ee5572156f348c4af
cadorn <christoph@christophdorn.com>
2011-07-20 13:45:38 -0700

    cleanup

--- workspace/lib/project.js
+++ workspace/lib/project.js
@@ -1,13 +1,5 @@
 
-var PATH = require("nodejs/path"),
-    PROGRAM_SERVER = require("pinf/program-server"),
-    LOADER = require("pinf/loader"),
-    Q = require("modules/q");
-
-
 exports.main = function(options)
 {
-	
-	console.log("Hello World!!");
 	
 }
